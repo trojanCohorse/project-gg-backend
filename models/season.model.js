143091898f79eca6fda5a9f9271c281542e3fb06
Asif <asif14.k@hotmail.com>
2020-12-30 18:34:27 -0500

    Setup test files

---
+++ models/season.model.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+// format data to post to the database
+const seasonSchema = new Schema({
+  // _id: { type: mongoose.Schema.Types.ObjectId },
+  season: { type: Number, required: true },
+  episodes: { type: Array, required: true }
+}, {
+  timestamps: true
+})
+
+const Season = mongoose.model('Season', seasonSchema);
+
+module.exports = Season;
+
+
+
+
+
+
+
+// example :
+// episodes: [
+//   season: 1,
+//   episode: 1,
+//   name: "Pilot",
+//   references: [
+//     "id": 1,
+//     "subject": "Jack Kerouac",
+//     "timestamp": "1:27",
+//     "quote": "You're a regular Jack Kerouac.",
+//     "speaker": "Lorelai",
+//     "context": "Lorelai is approached by a stranger who flirts with her at Luke's diner. He tells her  that he is just passing by Stars Hollow to Hartford, and this is Lorelai's response",
+//     "meaning": "Jack Kerouac was famous for his travels, which lead to his novel 'On The Road'.",
+//     "screenshot": "https://some-picture-hosting-website.com/image"
+//   ]
+// ]
+
+
+// const mongoose = require("mongoose")
+// const Schema = mongoose.Schema
+
+// const userSchema = new Schema({
+//   userName: {
+//     type: String,
+//     required: true,
+//     unique: true,
+//     trim: true,
+//     minlength: 3
+//   },
+// }, {
+//   timestamps: true,
+// })
+
+// const User = mongoose.model('User', userSchema)
+
+// module.exports = User
---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+// Creating express application
+const app = express();
+// Heroku will generate a env variable called process.env.port for our server to be hosted on
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established");
+});
+
+const seasonsRouter = require('./routes/seasons');
+
+app.use('/seasons', seasonsRouter);
+// app.use('/',)
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
+});
