2d4df26f4e183d44723c35183cde2b89b9180a1f
Asif <asif14.k@hotmail.com>
2020-12-31 10:54:38 -0500

    Deploying to heroku again twice

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
+const uri = 'mongodb+srv://ggadmin:thisIsOurConnectionStringPass30@gilmorecluster.49pdi.mongodb.net/GGData?retryWrites=true&w=majority';
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
 const connection = mongoose.connection;
 connection.once('open', () => {
